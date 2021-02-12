6832294396e9104b164eb2cb5f70709c133a581c
Fabrice Loru <34213953+adnduweb@users.noreply.github.com>
2021-02-12 19:33:09 +0100

    Update

---
+++ Helpers/CliHelper.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace Adnduweb\Ci4Core\Helpers;
+
+use Exception;
+
+class FileHelper
+{
+
+    protected static function _returnFalse($error, $throwExceptions)
+    {
+        if ($throwExceptions)
+        {
+            throw new Exception($error);
+        }
+
+        return false;
+    }
+
+    public static function setPermission($path, $permission, $throwExceptions = true, &$error = null)
+    {
+        if (is_file($path) || is_dir($path))
+        {
+            if (is_string($permission))
+            {
+                $permission = octdec($permission);
+            }
+
+            $result = chmod($path, $permission);
+
+            if (!$result)
+            {
+                $error = $path . ' chmod ' . $permission . ' error.';
+            
+                return static::_returnFalse($error, $throwExceptions);
+            }
+        }
+        else
+        {
+            $error = $path . ' path not found.';
+
+            return static::_returnFalse($error);
+        }
+
+        return true;
+    }
+
+    public static function delete($dir, $throwExceptions = true, &$error = null)
+    {
+        if (is_file($dir) || is_link($dir))
+        {
+            if (!unlink($dir))
+            {
+                $error = 'Can\'t delete: ' . $dir;
+
+                return static::_returnFalse($error, $throwExceptions);
+            }
+
+            return true;
+        }
+
+        if (!is_dir($dir))
+        {
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        $items = static::readDirectory($dir, $throwExceptions, $error);
+
+        if ($items === false)
+        {
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        foreach($items as $file)
+        {
+            if (!static::delete($dir . DIRECTORY_SEPARATOR . $file, $error))
+            {
+                return static::_returnFalse($error, $throwExceptions);
+            }
+        }
+
+        if (!rmdir($dir))
+        {
+            $error = 'Can\'t delete directory: '. $dir; 
+
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        return true;
+    }    
+
+    public static function createDirectory($path, $mode = 0775, $recursive = true, $throwExceptions = true, &$error = null)
+    {
+        if (is_dir($path))
+        {
+            return true;
+        }
+
+        $parentDir = dirname($path);
+        
+        if ($recursive && !is_dir($parentDir) && $parentDir !== $path)
+        {
+            $result = static::createDirectory($parentDir, $mode, true, $throwExceptions, $error);
+
+            if (!$result)
+            {
+                return static::_returnFalse($error, $throwExceptions);
+            }
+        }
+        
+        try
+        {
+            if (!mkdir($path, $mode))
+            {
+                $error = $path . ' mkdir error.';
+
+                static::_returnFalse($error, $throwExceptions);
+            }
+        }
+        catch(Exception $e)
+        {
+            if (!is_dir($path))
+            {
+                $error = "Failed to create directory \"$path\": " . $e->getMessage();
+
+                return static::_returnFalse($error, $throwExceptions);
+            }
+        }
+
+        return static::setPermission($path, $mode, $throwExceptions, $error);
+    }
+
+    public static function copySymlink($source, $dest, $throwExceptions = true, &$error = null)
+    {
+        if (!is_link($source))
+        {
+            return static::_returnFalse($source . ' not symlink.', $throwExceptions);
+        }
+
+        if (!symlink(readlink($source), $dest))
+        {
+            $error = 'Can\'t create symlink from ' . $source . ' to ' . $dest;
+
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        return true;
+    }
+
+    public static function copyFile($source, $dest, $permission = 0755, $throwExceptions = true, &$error = null)
+    {
+        if (!is_file($source))
+        {
+            return static::_returnFalse($source . ' not file.', $throwExceptions);
+        }
+
+        $dir = pathinfo($dest, PATHINFO_DIRNAME);
+
+        if (!$dir)
+        {
+            $error = 'Can\'t get PATHINFO_DIRNAME: ' . $dest;
+
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        if (!static::createDirectory($dir, $permission, true, $throwExceptions, $error))
+        {
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        if (!copy($source, $dest))
+        {
+            $error = 'Can\'t copy file from ' . $source . ' to ' . $dest;
+
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        return true;
+    }
+
+    public static function copyDirectory($source, $dest, $permission = 0755, $throwExceptions, &$error = null)
+    {
+        if (!is_dir($dest))
+        {        
+            if (!static::createDirectory($dest, $permission, true, $throwExceptions, $error))
+            {
+                return static::_returnFalse($error, $throwExceptions);
+            }
+        }
+
+        $items = static::readDirectory($source, $throwExceptions, $error);
+
+        if ($items === false)
+        {
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        foreach($items as $file)
+        {
+            if (!static::copy($source . DIRECTORY_SEPARATOR . $file, $dest . DIRECTORY_SEPARATOR . $file, $permission, $throwExceptions, $error))
+            {
+                return static::_returnFalse($error, $throwExceptions);
+            }
+        }
+
+        return true;
+    }
+
+    public static function copy($source, $dest, $permission = 0755, $throwExceptions = true, &$error = null)
+    {
+        if (is_link($source))
+        {
+            return static::copySymlink($source, $dest);
+        }
+
+        if (is_file($source))
+        {
+            return static::copyFile($source, $dest, $permission, $throwExceptions, $error);
+        }
+
+        if (is_dir($source))
+        {
+            return static::copyDirectory($source, $dest, $permission, $throwExceptions, $error);
+        }
+
+        $error = 'File not found: ' . $source;
+
+        return static::_returnFalse($error, $throwExceptions);
+    }
+
+    public static function readDirectory($source, $throwExceptions = true, &$error = null)
+    {
+        $dir = dir($source);
+
+        if (!$dir)
+        {
+            $error = 'Can\'t open directory: ' . $dir;
+
+            return static::_returnFalse($error, $throwExceptions);
+        }
+
+        $items = [];
+        
+        while(false !== ($file = $dir->read()))
+        {
+            if ($file == '.' || $file == '..')
+            {
+                continue;
+            }
+
+            $items[] = $file;
+        }
+
+        $dir->close();
+
+        return $items;
+    }
+
+}
